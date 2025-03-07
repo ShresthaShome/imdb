337c68840019fcb459a57d8ef0b6dfa2ab8c5a13
Shrestha Shome Ullas <shrestha.shome@gmail.com>
2025-03-08 03:45:27 +0600

    feat: fetching and displaying movies

--- src/app/top/[genre]/page.tsx
+++ src/app/top/[genre]/page.tsx
@@ -1,9 +1,11 @@
-/*import Results from "@/components/Results";
+import Results from "@/components/Results";
+
 const API_KEY = process.env.API_KEY;
+
 export default async function Home({
   params,
 }: {
-  params: Promise<{ id: string }>;
+  params: Promise<{ genre: string }>;
 }) {
   const { genre } = await params;
   const res = await fetch(
@@ -11,23 +13,17 @@ export default async function Home({
       genre === "rated" ? `/movie/top_rated` : `/trending/all/week`
     }?api_key=${API_KEY}&language=en-US&page=1`
   );
+
   const data = await res.json();
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
+
   const results = data.results;
 
   return (
     <div>
       <Results results={results} />
-    </div>
-  );
-}
-*/
-export default function page() {
-  return (
-    <div>
-      <h1>page</h1>
     </div>
   );
 }
